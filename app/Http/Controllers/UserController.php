117e40f3eb9ffeb246d65f679cb4259811739393
monmima <monmima@protonmail.com>
2022-10-17 21:02:45 -0400

    now at 3.45 in tutorial

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -96,4 +96,38 @@ class UserController extends Controller
     {
         //
     }
+
+    // logout
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerate();
+
+        return redirect("/")->with("message", "You have been logged out!");
+    }
+
+    // show login form
+    public function login()
+    {
+        return view ("users.login");
+    }
+
+    // authenticate user
+    public function authenticate(Request $request)
+    {
+        $formFields = $request->validate([
+            "email" => ["required", "email"],
+            "password" => ["required"]
+        ]);
+
+        if (auth()->attempt($formFields)) {
+            $request->session()->regenerate();
+
+            return redirect("/")->with("message", "You are now logged in!");
+        }
+
+        return back()->withErrors(["email" => "Invalid credentials"])->onlyInput("email ");
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -57,3 +57,12 @@ Route::get('/register',  [UserController::class, "create"]);
 
 // create new user
 Route::post('/users',  [UserController::class, "store"]);
+
+// log user out
+Route::post("/logout", [UserController::class, "logout"]);
+
+// show login form
+Route::get("/login", [UserController::class, "login"]);
+
+// log in user
+Route::post("/users/authenticate", [UserController::class, "authenticate"]);
